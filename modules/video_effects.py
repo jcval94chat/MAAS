3493b95cb3fc28380ae4e73ec3de9a7d22fe2548
José Carlos Del Valle <jcval94chat@gmail.com>
2025-03-31 10:09:04 -0600

    Quitar Kahoot

--- modules/video_effects.py
+++ modules/video_effects.py
@@ -1385,8 +1385,8 @@ def get_onomatos():
   '¿Eh?': ['Microsoft Windows XP Error'],
   'Zzzz': ['Roncar Ronquidos Efecto (man)',
     'Mujer que Ronca - Efecto de Sonido (HD) (woman)'],
-  'Hmm': ['Minecraft Villager (huh) - Sound Effect',
-    'KAHOOT Music (10 Second Countdown) 3_3'],
+  'Hmm': ['Minecraft Villager (huh) - Sound Effect',#'KAHOOT Music (10 Second Countdown) 3_3'
+    ],
   'Shhh': 'shhhhhhhhh sound',
   'Jaja': ['Ha Sound Effect (man)', 'Risa de ibai'],
   'Ay': 'Duck Toy Squeak Dog Toy Sound Effect (download)',
